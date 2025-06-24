cc09724b62004b2312c3ca067d53f44d7ca0f973
ce0960406e41de52 <2cb52d6d4dfc7aca>
2025-06-24 06:14:03 +0000

    update

--- src/cryptoagents_a2a_devkit/handlers.py
+++ src/cryptoagents_a2a_devkit/handlers.py
@@ -7,15 +7,21 @@ class AgentDetail(TypedDict):
     description: str
     agent_name: str
     base_url: str # something like http://api.openai.com/v1/chat/completions
+    status: str # "running" or "!running"
+    avatar_url: Optional[str]
 
 BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL", "http://localhost:8000")
 AUTHORIZATION_TOKEN = os.getenv("AUTHORIZATION_TOKEN", "super-secret") 
 
-async def get_agent_detail(agent_id: str) -> Optional[AgentDetail]:
+async def get_agent_detail(
+    agent_id: str,
+    backend_base_url: str = BACKEND_BASE_URL,
+    authorization_token: str = AUTHORIZATION_TOKEN,
+) -> Optional[AgentDetail]:
     """
     Get the details of an agent
     """
-    
+
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(
@@ -26,16 +32,19 @@ async def get_agent_detail(agent_id: str) -> Optional[AgentDetail]:
             return None
 
         if response.status_code == 200:
-            data = response.json()
+            data: dict = response.json()
+            meta_data: dict = data["meta_data"]
 
-            container = data["container_name"] or data["container_id"]
-            port = data["port"] or 80
+            container: str = data["container_name"] or data["container_id"]
+            port: int = data["port"] or 80
 
             return AgentDetail(
                 agent_id=agent_id,
-                description=data["description"],
-                agent_name=data["name"],
-                base_url=f"http://{container}:{port}/prompt"
+                description=data.get("description", ""),
+                agent_name=data.get("name", str(agent_id)),
+                base_url=f"http://{container}:{port}/prompt",
+                status=data.get("status", "unknown"),
+                avatar_url=meta_data.get("nft_token_image", None),
             )
 
         return None
